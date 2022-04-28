7c9c1af35dff07f66f4946e8af75eecff7f7bfac
Dooomand56 <doomand56@gmail.com>
2022-04-28 21:38:42 +0300

    created slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,29 @@
-console.log('Я люблю мою кицьку!');
+// Sroller
+
+let offset = 0;
+let scroll = document.querySelector('.wrap-img');
+
+document.querySelector('.btn-scroll-next').addEventListener('click', function scrollNext () {
+  offset = offset + 948;
+  if (offset > 3786) {
+  	offset = 0;
+  }
+  scroll.style.left = -offset + 'px';
+  console.log(offset);
+});
+document.querySelector('.btn-scroll-prev').addEventListener('click', function () {
+  offset = offset - 948;
+  if (offset < 0) {
+  	offset = 2844;
+  }
+  scroll.style.left = -offset + 'px';
+  console.log(offset);
+});
+
+setInterval(() => {
+  offset = offset + 948;
+  if (offset > 3786) {
+  	offset = 0;
+  }
+  scroll.style.left = -offset + 'px';
+}, 5000)
